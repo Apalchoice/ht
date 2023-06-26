0e9d56130ee1479aab30460c937af3744eacf506
eMobilis <alexrop19@gmail.com>
2023-06-26 13:48:57 +0300

    added file

--- data structures.py
+++ data structures.py
@@ -5,3 +5,25 @@ cars.append("jeep")
 cars.insert(2, "Mitsubishi")
 cars.sort()
 print(cars)
+
+
+# this is a tuple, ordered,its unchangeable
+fruits=("mangoes", "apples", "banana", "ovacado", "apples")
+my_tuple = (fruits)
+count=len(my_tuple)
+print(count)
+
+
+#sets datastructure,unordered
+countries = {"kenya", "uganda", "tanzania","burundi", "ethiopia"}
+print(countries)
+
+#dictinaries
+matunda ={
+    "amount":40,
+    "jina": "ndizi",
+    "rangi": "yellow"
+}
+matunda["size"]="large"
+matunda.pop("rangi")
+print(matunda)
